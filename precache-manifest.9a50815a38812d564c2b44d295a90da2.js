8495d467a3cba8ad1c4793ea16e2c18a042b9823
deepak577 <marketinsoft@gmail.com>
2020-05-09 12:17:49 +0530

    Updates

---
+++ precache-manifest.9a50815a38812d564c2b44d295a90da2.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553c21b9205869d55b01af1aa73eb59e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c0a7e832742f9150f35f",
+    "url": "/my-app/static/css/main.0c896399.chunk.css"
+  },
+  {
+    "revision": "0abdce58a978491f8af2",
+    "url": "/my-app/static/js/2.0fed5b06.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.0fed5b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a7e832742f9150f35f",
+    "url": "/my-app/static/js/main.26baca00.chunk.js"
+  },
+  {
+    "revision": "6b90711d51ae396ac389",
+    "url": "/my-app/static/js/runtime-main.8c451e69.js"
+  },
+  {
+    "revision": "9448e7f0fb7628344d37f74a60177b77",
+    "url": "/my-app/static/media/Akinchana krishna das babaji.9448e7f0.jpg"
+  },
+  {
+    "revision": "450744b9c344e0c13850deeb72b9823d",
+    "url": "/my-app/static/media/Ananda Gopal Goswami , Pratibha sundari Goswamini.450744b9.jpg"
+  },
+  {
+    "revision": "92fe0434eca65db34cb93f4d8947c9d2",
+    "url": "/my-app/static/media/Ananda Gopala Goswami.92fe0434.jpg"
+  },
+  {
+    "revision": "3cb03f205aa48a223fa4e59527bcd280",
+    "url": "/my-app/static/media/BV Sadhu Maharaj.3cb03f20.jpg"
+  },
+  {
+    "revision": "df0169527c99c2864eacca67e8f37fa1",
+    "url": "/my-app/static/media/Govardhan das babaji.df016952.jpg"
+  },
+  {
+    "revision": "fb896642f9c73afcb291e104898133ec",
+    "url": "/my-app/static/media/Mahavatar baba.fb896642.jpg"
+  },
+  {
+    "revision": "bad5fad8c0e2454141d174297260970b",
+    "url": "/my-app/static/media/Murari das babaji.bad5fad8.jpg"
+  },
+  {
+    "revision": "eb7e43d7c716c9ccbd11d4f9150c6692",
+    "url": "/my-app/static/media/Nikunja gopal Goswami 2.eb7e43d7.jpg"
+  },
+  {
+    "revision": "0d00aa77dceaaebeac00c4c76e141554",
+    "url": "/my-app/static/media/Nikunja gopal Goswami 3.0d00aa77.jpg"
+  },
+  {
+    "revision": "2f011aa3bb866dbeac2bac3bb62266aa",
+    "url": "/my-app/static/media/Nikunja gopal Goswami 4.2f011aa3.jpg"
+  },
+  {
+    "revision": "69a467d98b29cc460b0f64a8212e12d1",
+    "url": "/my-app/static/media/Nikunja gopal Goswami 5.69a467d9.jpg"
+  },
+  {
+    "revision": "82603b5f9c167751db00272285de8e72",
+    "url": "/my-app/static/media/Nikunja gopal Goswami.82603b5f.jpg"
+  },
+  {
+    "revision": "c0c497527a38696e3b0d5ac2006fd3d8",
+    "url": "/my-app/static/media/Pandit Vaishnavacharan das babaji.c0c49752.jpg"
+  },
+  {
+    "revision": "ba05ad5149e30d04320dbeaf4181b382",
+    "url": "/my-app/static/media/Radha raman charan das babaji.ba05ad51.jpg"
+  },
+  {
+    "revision": "d06471eb2d27350b06c834b2bcde7258",
+    "url": "/my-app/static/media/Radharaman charna das babaji 2.d06471eb.jpg"
+  },
+  {
+    "revision": "9f07b4b84a81ddf5ba1b629bed4593b7",
+    "url": "/my-app/static/media/Sachinandan das babaji (Bhai baba).9f07b4b8.jpg"
+  },
+  {
+    "revision": "44aa4db5da81c7295ad0cf4cda96a340",
+    "url": "/my-app/static/media/Siddha Jagadish das babaji.44aa4db5.jpg"
+  },
+  {
+    "revision": "ee9f5b9b77a4dcea69c43890f10be88e",
+    "url": "/my-app/static/media/Siddha manohar das babaji.ee9f5b9b.jpg"
+  },
+  {
+    "revision": "51109e12bdb623d0a387c6f46bb99786",
+    "url": "/my-app/static/media/Sri Advaita das babaji.51109e12.jpg"
+  },
+  {
+    "revision": "18221aebab1bbfd3e1d17badbeb73dbf",
+    "url": "/my-app/static/media/Sri Gauragunananda Thakur.18221aeb.jpg"
+  },
+  {
+    "revision": "718785a69f802507499b0d6c1323c37e",
+    "url": "/my-app/static/media/Sri Gauranga das babaji 2.718785a6.jpg"
+  },
+  {
+    "revision": "9331de0420dff194bcdc664b255613e0",
+    "url": "/my-app/static/media/Sri Gauranga das babaji.9331de04.jpg"
+  },
+  {
+    "revision": "df0169527c99c2864eacca67e8f37fa1",
+    "url": "/my-app/static/media/Sri Govardhan das babaji.df016952.jpg"
+  },
+  {
+    "revision": "52178ef2f972d640f2b00daafc6bad4a",
+    "url": "/my-app/static/media/Sri Haridas Sastri 2.52178ef2.jpg"
+  },
+  {
+    "revision": "278935fea10ec5b9fc52999632f1ce42",
+    "url": "/my-app/static/media/Sri Haridas Sastri.278935fe.jpg"
+  },
+  {
+    "revision": "c72ddc8991908fce1e3738e8e2716dc7",
+    "url": "/my-app/static/media/Sri Haridas das babaji.c72ddc89.jpg"
+  },
+  {
+    "revision": "3e0b84c64b89786dbfc90e1556de16b3",
+    "url": "/my-app/static/media/Sri Hridayananda babaji.3e0b84c6.jpg"
+  },
+  {
+    "revision": "6806bfb99198b4c56aec9db13d62f752",
+    "url": "/my-app/static/media/Sri Jagannath das babaji.6806bfb9.jpg"
+  },
+  {
+    "revision": "a339f9cd3d16945d9cf5f5c386bc5bb1",
+    "url": "/my-app/static/media/Sri Kunjabihari das babaji & Ananta das babaji.a339f9cd.jpg"
+  },
+  {
+    "revision": "8092c86051606eac0dbad19f5177d5e1",
+    "url": "/my-app/static/media/Sri Kunjabihari das babaji.8092c860.jpg"
+  },
+  {
+    "revision": "44afdddf872703708051f91170b7c878",
+    "url": "/my-app/static/media/Sri Lalit prasad Thakur.44afdddf.jpg"
+  },
+  {
+    "revision": "9b91756a5a92268b60eb01f23ad98d7e",
+    "url": "/my-app/static/media/Sri Madhusudan das babaji 2.9b91756a.jpg"
+  },
+  {
+    "revision": "0818ea04b83cd3ead38c3f3bf9ab9a06",
+    "url": "/my-app/static/media/Sri Madhusudan das babaji.0818ea04.jpg"
+  },
+  {
+    "revision": "5c7d492261b37d0fbea2d85e0b8f8ab3",
+    "url": "/my-app/static/media/Sri Radharman charan das babaji.5c7d4922.jpg"
+  },
+  {
+    "revision": "66cf43d3049e067a3a3a9ff5267a10dd",
+    "url": "/my-app/static/media/Sri Ramdas Babaji.66cf43d3.jpg"
+  },
+  {
+    "revision": "70e1e5cc6deec7d34f24711061e16a03",
+    "url": "/my-app/static/media/Sri Ramdas babaji 3.70e1e5cc.jpg"
+  },
+  {
+    "revision": "fff1bb0271c02bd5f75d7d069c6ec198",
+    "url": "/my-app/static/media/Sri Ramdas das  babaji.fff1bb02.jpg"
+  },
+  {
+    "revision": "2d44aec61a471c2b97e26d416d4e1936",
+    "url": "/my-app/static/media/Sri kunjabihari das babaji 2.2d44aec6.jpg"
+  },
+  {
+    "revision": "55b175f709fbafd850da120e6edeefe7",
+    "url": "/my-app/static/media/Sri nityananda das babaji.55b175f7.jpg"
+  },
+  {
+    "revision": "c9a3670e48a0378ca8006bbbfd045d90",
+    "url": "/my-app/static/media/Srila Ramakrishna das ji maharaj.c9a3670e.jpg"
+  },
+  {
+    "revision": "8d7d3b88103be9913e0c778724b88daf",
+    "url": "/my-app/static/media/Srimati Yogamaya devi.8d7d3b88.jpg"
+  },
+  {
+    "revision": "ee90e09c495129fdefd7d243e67d2f02",
+    "url": "/my-app/static/media/Tinkudi babaji.ee90e09c.jpg"
+  },
+  {
+    "revision": "9750e2f6c2ef5734ec7774e8f2d60842",
+    "url": "/my-app/static/media/Vaisnava pada das babaji.9750e2f6.jpg"
+  },
+  {
+    "revision": "07c90b345ad20efad73df5058f6ad77d",
+    "url": "/my-app/static/media/Vijay Krishna das babaji.07c90b34.jpg"
+  },
+  {
+    "revision": "0f4e40f849c8e7779279ca45d5d8a2fb",
+    "url": "/my-app/static/media/Vijay krishna das babaji 2.0f4e40f8.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "970d970b811775941dfe8a7321203895",
+    "url": "/my-app/static/media/sri Vamsi das babaji.970d970b.jpg"
+  }
+]);
